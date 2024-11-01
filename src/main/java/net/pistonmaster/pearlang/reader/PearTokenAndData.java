b6449c244774d6a53532abd1c5f24e3f7caae142
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-11-01 11:49:55 +0100

    Add from and to column data

--- src/main/java/net/pistonmaster/pearlang/reader/PearTokenAndData.java
+++ src/main/java/net/pistonmaster/pearlang/reader/PearTokenAndData.java
@@ -1,4 +1,7 @@
 package net.pistonmaster.pearlang.reader;
 
-public record PearTokenAndData(PearToken token, String data, int line, int column) {
+public record PearTokenAndData(PearToken token, String data, int line, int columnFrom, int columnTo) {
+    public PearTokenAndData(PearToken token, String data, int line, int columnTo) {
+        this(token, data, line, columnTo - data.length(), columnTo);
+    }
 }
